c23cd36651e85e493f981a1d1bcdf3a8b8ce88e8
sbudiman12 <146686340+sbudiman12@users.noreply.github.com>
2024-04-19 13:29:12 +0700

    Initial Commit

---
+++ LandmarkApp/LandmarkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkAppApp.swift
+//  LandmarkApp
+//
+//  Created by STVN on 19/04/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
