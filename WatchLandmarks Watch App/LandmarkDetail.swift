1e86c5dba69744eae4453d350db7c0e8cbbc077b
sbudiman12 <146686340+sbudiman12@users.noreply.github.com>
2024-05-02 18:12:28 +0700

    commit

---
+++ WatchLandmarks Watch App/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  WatchLandmarks Watch App
+//
+//  Created by STVN on 02/05/24.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
---
+++ WatchLandmarks Watch App/NotificationView.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationView.swift
+//  WatchLandmarks Watch App
+//
+//  Created by STVN on 02/05/24.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationView()
+}
