cbf925cbb8afd90dbf18147286ba1bf69247e714
sbudiman12 <146686340+sbudiman12@users.noreply.github.com>
2024-05-02 20:17:21 +0700

    commit

---
+++ MacLandmarks/LandmarkSettings.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkSettings.swift
+//  MacLandmarks
+//
+//  Created by STVN on 02/05/24.
+//
+
+import SwiftUI
+
+struct LandmarkSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkSettings()
+}
